b5082729d2f09042b4d032964d89bc56e6708d4a
Galstian Vagan <gvagan@me.com>
2026-02-02 17:45:37 +0700

    Initial Commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Ваган Галстян on 02.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
